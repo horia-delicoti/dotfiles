f74278bc91a5333c8b0095e2226301dab1b3c76d
Horia Delicoti <hdelicoti@bitdefender.com>
2019-12-05 11:42:20 +0200

    Working on downloading the tarball image

---
+++ src/os/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declare -r GITHUB_REPOSITORY="horia-delicoti/dotfiles"
+
+declare -r DOTFILES_TARBALL_URL="https://github.com/horia-delicoti/dotfiles/tarball/master"
+
+echo $DOTFILES_TARBALL_URL
+
+main() {
+    echo "Test"    
+}
