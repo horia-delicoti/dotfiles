a6fced967b299339e87cfde1ac21d4f805795f86
Horia Delicoti <hdelicoti@bitdefender.com>
2019-12-06 11:18:04 +0200

    Add utils file

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-#declare -r GITHUB_REPOSITORY="horia-delicoti/dotfiles"
+# "declare" - typing variable; permits modifying the properties of variables
+# "-r" - readonly; this is a rough equivalent of the C "const" type
+declare -r GITHUB_REPOSITORY="horia-delicoti/dotfiles"
+declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/master"
 
-declare -r DOTFILES_TARBALL_URL="https://github.com/horia-delicoti/dotfiles/tarball/master"
 
-echo $DOTFILES_TARBALL_URL
+download_utils() {
+
+    local tmpFile=""
+
+    # mktemp - creates  a  temporary  file  or  directory
+    tmpFile="$(mktemp /tmp/XXXXX)"
+    
+    echo $tmpFile
+
+    return 1
+}
+
+
+# -----------------------------------------------
+# | Main                                        |
+# -----------------------------------------------
 
 main() {
-    echo "Test"    
+    echo $DOTFILES_TARBALL_URL
+
+    # Load utils
+    download_utils
 }
+
+main
