553cdd133f81e440d6a02a30118862b556d2b48e
Horia Delicoti <horia.delicoti@gmail.com>
2021-12-07 17:18:35 +0200

    os/setup.sh: Download tarball url

--- src/os/utils.sh
+++ src/os/utils.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo "this is bash"
+print_in_color() {
+    # printf - has more control over the formatting of the output
+    printf "%b" "$(tput setaf "$2" 2> /dev/null)" "$1" "$(tput sgr0 2> /dev/null)"
+        #           |    └─ Set foreground color: 1: Red, 2: Green, 3: Yellow, 4: Blue, 5: Magenta
+        #           └─ Command used to manipulate terminal, change color of text, apply effects
+}
+
+print_in_green() {
+    print_in_color "$1" 2
+}
+
+print_in_purple() {
+    print_in_color "$1" 5
+}
+
+print_in_red() {
+    print_in_color "$1" 1
+}
+
+print_in_yellow() {
+    print_in_color "$1" 3
+}
+
+print_in_green "test"
