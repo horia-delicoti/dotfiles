aacc0496e682bf8175bda88cb87d584e1db984cb
Horia Delicoti <horia.delicoti@gmail.com>
2023-03-21 17:09:58 +0200

    Enable browser install

--- src/os/installs/macos/browsers.sh
+++ src/os/installs/macos/browsers.sh
@@ -10,7 +10,7 @@ print_in_purple "\n   Browsers\n"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-printf "\n"
+#printf "\n"
 
 #brew_install "Chrome" "google-chrome" "--cask"
 #brew_install "Chrome Canary" "google-chrome-canary" "--cask" "homebrew/cask-versions"
@@ -29,7 +29,7 @@ brew_install "Firefox" "firefox" "--cask"
 # `Safari Technology Preview` requires macOS 10.11.4 or la
 # https://github.com/alrra/dotfiles/issues
 
-if is_supported_version "$(get_os_version)" "10.11.4"; then
-    printf "\n"
+#if is_supported_version "$(get_os_version)" "10.11.4"; then
+#    printf "\n"
     #brew_install "Safari Technology Preview" "safari-technology-preview" "--cask" "homebrew/cask-versions"
-fi
+#fi
