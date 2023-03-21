0a2ff5fb4fc1a2826c225d5d9d83136bbfd2a8cb
Horia Delicoti <horia.delicoti@gmail.com>
2023-03-21 15:47:17 +0200

    Add instalation for Whatsapp

---
+++ src/os/preferences/macos/whatsapp.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_whatsapp() {
+
+    # If necessary, prompt user to install `Whatsapp`.
+
+    if ! is_whatsapp_installed; then
+        open "macappstores://itunes.apple.com/en/app/whatsapp-desktop/id1147396723"
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until `Whatsapp` is installed.
+
+    execute \
+        "until is_whatsapp_installed; do \
+            sleep 5; \
+         done" \
+        "WhatsApp.app"
+
+}
+
+is_whatsapp_installed() {
+    [ -d "/Applications/WhatsApp.app" ]
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "   Xcode\n\n"
+
+    install_whatsapp
+
+}
+
+main
