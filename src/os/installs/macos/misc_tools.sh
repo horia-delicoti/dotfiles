802f10c83e7b4990d13cca7e9575e6c47bddb7d8
Horia Delicoti <horia.delicoti@gmail.com>
2023-04-03 12:05:55 +0300

    misc_tools.sh: Add brew go-task installation

--- src/os/installs/macos/misc_tools.sh
+++ src/os/installs/macos/misc_tools.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://formulae.brew.sh/
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../../utils.sh" \
@@ -10,6 +11,7 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 
 #brew_install "ShellCheck" "shellcheck"
 brew_install "Wget" "wget"
+brew_install "Go-Task" "go-task"
 #brew_install "Terraform" "terraform"
 #brew_install "Docker" "docker"
 brew_install "Openvpn" "openvpn"
