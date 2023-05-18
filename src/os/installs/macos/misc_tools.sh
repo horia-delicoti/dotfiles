9cd59147a57939ceebb9fd6159a0338851b6654b
Horia Delicoti <horia.delicoti@gmail.com>
2023-05-18 12:34:59 +0300

    misc_tools.sh: Add brew install for htop

--- src/os/installs/macos/misc_tools.sh
+++ src/os/installs/macos/misc_tools.sh
@@ -11,6 +11,7 @@ print_in_purple "\n   Miscellaneous Tools\n\n"
 
 #brew_install "ShellCheck" "shellcheck"
 brew_install "Wget" "wget"
+brew_install "Htop" "htop"
 brew_install "Go-Task" "go-task"
 brew_install "Terraform" "terraform"
 #brew_install "Docker" "docker"
